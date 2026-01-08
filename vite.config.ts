78ff8a074e5d65256b8e8705c45b1c494e577c9c
sunnykhattri9621-prog <sunny.khattri9621@gmail.com>
2026-01-08 20:26:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react() ,  tailwindcss()],
-  server: {
-    port: 5173,
-    strictPort: false,
-    open: true,
-    mimeTypes: {
-      'tsx': 'text/javascript',  // ← ADD THIS
-      'ts': 'text/javascript',
-    }
+  build: {
+    outDir: 'dist'  // Ensure this
   }
 })
